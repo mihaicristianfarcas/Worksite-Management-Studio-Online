c839784f7fe2923e6dcad142a5fefade70e6cd68
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-05-16 21:26:54 +0300

    frontend ready for prod

--- frontend/src/components/admin/admin-filters-bar.tsx
+++ frontend/src/components/admin/admin-filters-bar.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import { ChevronDown, RefreshCcwDot, Search } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,10 @@
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current directory
-  const env = loadEnv(mode, process.cwd(), '')
-
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
@@ -19,7 +16,7 @@ export default defineConfig(({ mode }) => {
     base: '/',
     // Define environment variables to expose to the client
     define: {
-      'process.env': env
+      'process.env': process.env
     },
     // Build configuration
     build: {
