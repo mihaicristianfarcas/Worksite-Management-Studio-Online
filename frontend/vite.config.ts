3d3a88b53b17acb95558064b9aa500d9e303eb41
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-05-16 21:46:57 +0300

    modified vite config and dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
@@ -16,7 +20,7 @@ export default defineConfig(({ mode }) => {
     base: '/',
     // Define environment variables to expose to the client
     define: {
-      'process.env': process.env
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
     },
     // Build configuration
     build: {
